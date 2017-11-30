cbf5332535f1075354fc08a79a340c393aec3ba1
Daria Knyazeva <daria@bitposter.co>
2017-11-30 14:27:05 +0600

    Heroku database config

--- youth_football_club/settings.py
+++ youth_football_club/settings.py
@@ -139,3 +139,8 @@ LOGIN_REDIRECT_URL = '/'
 
 # pagination
 RESULTS_PER_PAGE = 1
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
